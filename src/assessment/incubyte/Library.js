240d90997f395e4ee50ac844f40664140604de76
darshilbaldha <darshilbaldha456@gmail.com>
2024-08-31 16:04:28 +0530

    refactored library name validation

--- src/assessment/incubyte/Library.js
+++ src/assessment/incubyte/Library.js
@@ -1,23 +1,27 @@
 class Library {
-    constructor(...args) {
-        if (args.length === 0) {
-            throw new Error("Library Can't be Created Without Name");
-        }
+  constructor(...args) {
+    if (args.length === 0) {
+      throw new Error("Library Can't be Created Without Name");
+    }
 
-        if (args.length === 1 && typeof args[0] === 'string') {
-            const libName = args[0];
-            if (libName.length < 4) {
-                throw new Error("Library name must be at least 4 characters long");
-            } else {
-                this.libName = libName;
-            }
-        } else if (args.length > 1) {
-            throw new Error("Constructor should be called with only one argument");
-        }
+    if (args.length === 1 && typeof args[0] === "string") {
+      const libName = args[0];
+      if (this.isNameValid(libName)) {
+        this.libName = libName;
+      } else {
+        throw new Error("Library name must be at least 4 characters long");
+      }
+    } else if (args.length > 1) {
+      throw new Error("Constructor should be called with only one argument");
     }
-    getName() {
-        return this.libName;
-    }
+  }
+  isNameValid(libName) {
+    return libName !== null && libName.length >= 4;
+  }
+
+  getName() {
+    return this.libName;
+  }
 }
 
 export default Library;
