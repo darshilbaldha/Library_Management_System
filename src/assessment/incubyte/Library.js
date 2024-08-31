ff4f5f71a20b735fd95e814133756585dc09040c
darshilbaldha <darshilbaldha456@gmail.com>
2024-08-31 11:08:03 +0530

    passed prev tests : Constructor with array of values must raise unsup exp

--- src/assessment/incubyte/Library.js
+++ src/assessment/incubyte/Library.js
@@ -1,7 +1,18 @@
 class Library {
-    constructor() {
-        if (arguments.length === 0) {
+    constructor(...args) {
+        if (args.length === 0) {
             throw new Error("Library Can't be Created Without Name");
+        }
+
+        if (args.length === 1 && typeof args[0] === 'string') {
+            const libName = args[0];
+            if (libName.length < 4) {
+                throw new Error("Library name must be at least 4 characters long");
+            } else {
+                this.libName = libName;
+            }
+        } else if (args.length > 1) {
+            throw new Error("Constructor should be called with only one argument");
         }
     }
 }
