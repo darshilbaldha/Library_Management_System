e4e88183712d8349b4a4c5930900fb86b9e5fc60
darshilbaldha <darshilbaldha456@gmail.com>
2024-09-01 21:02:41 +0530

    added test case for validating borrow book throws InvalidBookException when first parameter is other than book or ISBN

---
+++ src/assessment/incubyte/utils/static.js
@@ -0,0 +1,3 @@
+// constants.js
+export const LIB_NAME_1 = "New Library";
+export const LIB_NAME_2 = "Gujarat University Library";
--- tests/libraryTest/TestLibrary.test.js
+++ tests/libraryTest/TestLibrary.test.js
@@ -10,6 +10,7 @@ import LibraryInitialisationException from "../../src/assessment/incubyte/except
 import IllegalArgumentException from "../../src/assessment/incubyte/exception/IllegalArgumentException";
 import BorrowLimitExceededException from "../../src/assessment/incubyte/exception/BorrowLimitExceededException";
 import InvalidReturnAttemptException from "../../src/assessment/incubyte/exception/InvalidReturnAttemptException";
+import { LIB_NAME_1,LIB_NAME_2 } from "../../src/assessment/incubyte/utils/static";
 
 describe("Library Tests", () => {
   test("should throw LibraryInitialisationException with no arguments", () => {
@@ -25,26 +26,26 @@ describe("Library Tests", () => {
   });
 
   test("should create Library with valid name", () => {
-    const libName = "New Library";
+    const libName = LIB_NAME_1;
     const lib = new Library(libName);
     expect(lib.getName()).toBe(libName);
   });
 
   test("should throw LibraryInitialisationException with multiple arguments", () => {
     expect(
-      () => new Library("New Library", "Gujarat University Library")
+      () => new Library(LIB_NAME_1, LIB_NAME_2)
     ).toThrow(LibraryInitialisationException);
   });
 
   test("Testing Initialization Of Object with getName Function", () => {
-    const libName = "New Library";
+    const libName = LIB_NAME_1;
     const lib = new Library(libName);
     expect(lib).not.toBeNull();
     expect(lib.getName()).toBe(libName);
   });
 
   test("Test That addBook Method returns true when a new book is added to library", () => {
-    const libName = "New Library";
+    const libName = LIB_NAME_1;
     const lib = new Library(libName);
     const ISBN = "1234567890";
     const publicationYear = 2000;
@@ -59,13 +60,13 @@ describe("Library Tests", () => {
   });
 
   test("Test That getAvlBooks Method returns empty array when there are no books in the library", () => {
-    const libName = "New Library";
+    const libName = LIB_NAME_1;
     const lib = new Library(libName);
     expect(lib.getAvlBooks()).toEqual([]);
   });
 
   test("addBook method should add the book and only that book to the library in order", () => {
-    const libName = "New Library";
+    const libName = LIB_NAME_1;
     const lib = new Library(libName);
     const ISBN = "1234567890";
     const publicationYear = 2000;
@@ -83,13 +84,13 @@ describe("Library Tests", () => {
   });
 
   test("should throw InvalidBookException if addBook is called with null book", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const usr = new User("Darshil");
     expect(() => lib.addBook(null, usr)).toThrow(InvalidBookException);
   });
 
   test("should throw InvalidUserException if addBook is called with null user", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook = new Book(
       "1234567890",
       "Web Development",
@@ -100,7 +101,7 @@ describe("Library Tests", () => {
   });
 
   test("should throw InvalidUserException when borrowBook is called with null user", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook = new Book(
       "1234567890",
       "Web Development",
@@ -111,7 +112,7 @@ describe("Library Tests", () => {
   });
 
   test("addBook method should not add duplicate books again", () => {
-    const libName = "New Library";
+    const libName = LIB_NAME_1;
     const lib = new Library(libName);
     const ISBN = "1234567890";
     const publicationYear = 2000;
@@ -129,7 +130,7 @@ describe("Library Tests", () => {
   });
 
   test("addBook method should register a user if not registered previously", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const ISBN = "1234567890";
     const publicationYear = 2000;
     const testBook = new Book(
@@ -145,18 +146,18 @@ describe("Library Tests", () => {
   });
 
   test("should throw InvalidBookException when borrowBook is called with null book", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const usr = new User("Darshil");
     expect(() => lib.borrowBook(null, usr)).toThrow(InvalidBookException);
   });
 
   test("should throw IllegalArgumentException when borrowBook is called with null book and user", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     expect(() => lib.borrowBook(null, null)).toThrow(IllegalArgumentException);
   });
 
   test("should throw BookNotAvailableException when borrowBook is called with empty bookContainer", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook = new Book(
       "1234567890",
       "Web Development",
@@ -170,7 +171,7 @@ describe("Library Tests", () => {
   });
 
   test("borrowBook method should decrement book count by 1 when called with proper parameters", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook = new Book(
       "1234567890",
       "Web Development",
@@ -185,7 +186,7 @@ describe("Library Tests", () => {
   });
 
   test("borrowBook method does not decrement book count when count is already zero", () => {
-    const libName = "New Library";
+    const libName = LIB_NAME_1;
     const lib = new Library(libName);
     const ISBN = "1234567890";
     const publicationYear = 2000;
@@ -207,7 +208,7 @@ describe("Library Tests", () => {
   });
 
   test("borrowBook method registers a user and only that user if not registered previously", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook = new Book(
       "1234567890",
       "Web Development",
@@ -223,7 +224,7 @@ describe("Library Tests", () => {
   });
 
   test("borrowBook method logs successful borrowing (logging user & book)", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook = new Book(
       "1234567890",
       "Web Development",
@@ -239,7 +240,7 @@ describe("Library Tests", () => {
   });
 
   test("Test that borrowBook method limits the max borrowing numbers to specified value", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook1 = new Book(
       "1234567890",
       "Web Development 1",
@@ -270,7 +271,7 @@ describe("Library Tests", () => {
   });
 
   test("returnBook method throws expected exceptions for invalid scenarios", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook = new Book(
       "1234567890",
       "Web Development",
@@ -284,7 +285,7 @@ describe("Library Tests", () => {
   });
 
   test("Test That returnBook Method Throws InvalidReturnAttemptException with Invalid User Or Book", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook = new Book(
       "1234567890",
       "Web Development",
@@ -312,7 +313,7 @@ describe("Library Tests", () => {
   });
 
   test("should allow only a single copy of any book to be borrowed by a single user", () => {
-    const lib = new Library("New Library");
+    const lib = new Library(LIB_NAME_1);
     const testBook = new Book(
       "1234567890",
       "Web Development",
@@ -328,7 +329,7 @@ describe("Library Tests", () => {
   });
 
   test("should accept book object and ISBN for borrowing books", () => {
-    const libName = "New Library";
+    const libName = LIB_NAME_1;
     const lib = new Library(libName);
     const usr = DemoTestUserObjectStore.getUserObject(4);
     const testBook = DemoTestBookObjectStore.getBookObject(6);
@@ -336,5 +337,15 @@ describe("Library Tests", () => {
     expect(lib.borrowBook(testBook, usr)).toBe(true);
     lib.returnBook(testBook, usr);
     expect(lib.borrowBook(testBook.getISBN(), usr)).toBe(true);
+  });
+
+  test("throws InvalidBookException if parameters passed are other than ISBN or Book type", () => {
+    const lib = new Library("libName1");
+    const usr = DemoTestUserObjectStore.getUserObject(4);
+    const testBook = DemoTestBookObjectStore.getBookObject(6);
+    lib.addBook(testBook, usr);
+    expect(() => lib.borrowBook({}, usr)).toThrowError(
+      new InvalidBookException("Book Parameter Is Either Null Or Invalid")
+    );
   });
 });
